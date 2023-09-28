4ec1011b68438f73dfe11a583e09f91af23e9381
Davi Rodriguez <davi.rodriguez@inova.ind.br>
2023-09-28 11:34:13 -0300

    adicionado pgAdmin no docker compose

--- src/configs/postgres.config.ts
+++ src/configs/postgres.config.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
+
 @Injectable()
 export class PostgresConfigService implements TypeOrmOptionsFactory {
     constructor(private configService: ConfigService) { }
-    createTypeOrmOptions(): TypeOrmModuleOptions {
+    createTypeOrmOptions(): TypeOrmModuleOptions {       
         return {
             type: 'postgres',
             host: this.configService.get<string>('DB_HOST'),
@@ -13,7 +14,7 @@ export class PostgresConfigService implements TypeOrmOptionsFactory {
             username: this.configService.get<string>('DB_USER'),
             password: this.configService.get<string>('DB_PASSWORD'),
             database: this.configService.get<string>('DB_NAME'),
-            entities: [],
+            entities: [__dirname + '../**/*.entity{.js,.ts}'],  
             synchronize: true,
         };
     }
--- src/usuario/user.entity.ts
+++ src/usuario/user.entity.ts
@@ -1,6 +1,22 @@
+import { Entity, Column, CreateDateColumn, DeleteDateColumn, UpdateDateColumn, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity({ name: "users" })
 export class UserEntity {
+
+    @PrimaryGeneratedColumn('uuid')
     id: string;
+    @Column({ name: 'name', length: 100, nullable: false })
     nome: string;
+    @Column({ name: 'email', length: 70, nullable: false })
     email: string;
+    @Column({ name: 'password', length: 255, nullable: false })
     senha: string;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: string;
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt: string;
+    @DeleteDateColumn({ name: 'deleted_at' })
+    deletedAt: string;
+
 }
